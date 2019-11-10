c991634994193cafe39c01a0c0998a65f70c18a7
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-10 15:17:38 -0600

    Add: Initial HTML Content to page

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-$_GET = 'Steven';
-
 function greeting(){
     $name = $_GET['name'];
     echo "Hello" . $name;  
@@ -9,4 +7,20 @@ function greeting(){
 greeting();
 
 ?>
+<!DOCTYPE html> 
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>GET Superglobal</title>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
 
